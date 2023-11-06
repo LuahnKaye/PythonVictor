f4a022532e772ecb173af71d367b8d3d51d405a6
Luahn <115737683+LuahnKaye@users.noreply.github.com>
2023-11-05 21:34:28 -0300

    Update AC6-exercicio3.py

--- Trabalho AC6 victor/AC6-exercicio3.py
+++ Trabalho AC6 victor/AC6-exercicio3.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 import locale
 
-# Define o local para o português do Brasil
 locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
 
 def data_para_formato_extenso(data):
@@ -12,13 +11,10 @@ def data_para_formato_extenso(data):
     except ValueError:
         return None
 
-# Solicita a entrada do usuário
 data_input = input("Digite uma data no formato DD/MM/AAAA: ")
 
-# Chama a função com a data inserida pelo usuário
 data_formatada = data_para_formato_extenso(data_input)
 
-# Verifica se a data é válida e imprime o resultado
 if data_formatada:
     print(data_formatada)
 else:
